9a2b25c83c63e0fca005c017c1806b56a1e31131
JimCampagno <jimcampagno@gmail.com>
2015-08-27 15:43:10 -0400

    mapWithOperation tested

--- arrayBlocksTests/NSArray+EnumerationSpec.m
+++ arrayBlocksTests/NSArray+EnumerationSpec.m
@@ -18,119 +18,101 @@ describe(@"NSArray+Enumeration", ^{
     
     __block NSArray *orderedArray;
     __block NSArray *evenArray;
-    __block NSPredicate *evenPredicate;
-    
-    
-    
-    
-    //    NSArray *testArray = @[@1,@2,@3,@4,@5];
-    //
-    //    NSArray *resultsArray = [testArray mapWithOperation:^id(id object) {
-    //        return @([(NSNumber *)object integerValue] + 1);
-    //    }];
-    //
-    //    NSLog(@"resultsArray: %@",resultsArray);
-    //
-    //
-    //    NSLog(@"enumerateObjectsUsingBlock:");
-    //    [testArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-    //        NSLog(@"%@",@([(NSNumber *)obj integerValue] + 1));
-    //    }];
-    //
-    //
-    //    NSArray *filteredResultsArray = [resultsArray selectWithComparator:^BOOL(id object) {
-    //
-    //        if ([object integerValue]%2 == 0)
-    //        {
-    //            return YES;
-    //        }
-    //        else
-    //        {
-    //            return NO;
-    //        }
-    //    }];
-    //
-    //    NSLog(@"filteredResultsArray: %@", filteredResultsArray);
-    //
-    //    NSPredicate *evenPredicate = [NSPredicate predicateWithBlock:^BOOL(id evaluatedObject, NSDictionary *bindings){
-    //        return ([evaluatedObject integerValue]%2 == 0);
-    //    }];
-    //
-    //    NSArray *filteredResultsArrayTwo = [resultsArray filteredArrayUsingPredicate:evenPredicate];
-    //
-    //    NSLog(@"filteredResultsArrayTwo: %@",filteredResultsArrayTwo);
-    
-    
-    //    - (NSArray *)mapWithOperation:(id (^) (id object))operation;
-    //    - (NSArray *)selectWithComparator:(BOOL (^) (id object))operation;
-    
-    beforeAll(^{
-        
-    });
+    __block NSArray *nameArray;
     
     beforeEach(^{
         orderedArray = @[@1, @2, @3, @4, @5, @6];
         evenArray = @[@2, @4, @6, @8, @10];
-        evenPredicate = [NSPredicate predicateWithBlock:^BOOL(id evaluatedObject, NSDictionary *bindings){
-            return ([evaluatedObject integerValue] % 2 == 0);
-        }];
+        nameArray = @[@"Jim", @"Tom", @"Tim", @"Joe"];
     });
     
     describe(@"mapWithOperation:", ^{
         
-        it(@"It should iterate over an array similiar to the enumerateObjectsUsingBlock: method and return a NSArray which will iterate over the array, increase the NSNumber stored in the array by 1.", ^{
+        it(@"Should iterate over an array similiar to the enumerateObjectsUsingBlock: method and return a NSArray.  The argument being a block should be called correctly within the implementation of the mapWithOperation method doing what it's expected to do, then returning a NSArray object after having performed what has been defined/returned from the block.", ^{
             
-            NSArray *newArray = [orderedArray mapWithOperation:^id(id object) {
-                
-                return @[@"Hi"];
-//                return @([(NSNumber *)object integerValue] + 1);
+            NSArray *testArray = [orderedArray mapWithOperation:^id(id object) {
+                return @([(NSNumber *)object integerValue] * 2);
             }];
             
-            expect(newArray).to.beAKindOf([NSArray class]);
-            expect([newArray count]).to.equal([orderedArray count]);
-            expect(newArray).to.equal(@[@2, @3, @4, @5, @6, @7]);
-            expect(newArray).toNot.equal(@[@1, @2, @3, @4, @5, @6]);
+            NSArray *simpleArray = [orderedArray mapWithOperation:^id(id object) {
+                return object;
+            }];
             
+            NSArray *stringArray = [nameArray mapWithOperation:^id(id object) {
+                return [NSString stringWithFormat:@"%@ loves to code.", object];
+            }];
             
+            expect(testArray).to.beAKindOf([NSArray class]);
+            expect([testArray count]).to.equal([orderedArray count]);
+            expect(testArray).to.equal(@[@2, @4, @6, @8, @10, @12]);
+            expect(testArray).toNot.equal(@[@1, @2, @3, @4, @5, @6]);
             
+            expect(simpleArray).to.beAKindOf([NSArray class]);
+            expect([simpleArray count]).to.equal([orderedArray count]);
+            expect(simpleArray).to.equal(orderedArray);
             
-            
-            
-//            - (NSArray *)mapWithOperation:(id (^) (id object))operation {
-//                
-//                NSMutableArray *processingArray = [[NSMutableArray alloc] init];
-//                
-//                for (id object in self)
-//                {
-//                    [processingArray addObject:operation(object)];
-//                }
-//                
-//                return processingArray;
-            
-            
-            
-            
+            expect(stringArray).to.beAKindOf([NSArray class]);
+            expect([stringArray count]).to.equal([nameArray count]);
+            expect(stringArray).to.equal(@[@"Jim loves to code.",
+                                           @"Tim loves to code.",
+                                           @"Tom loves to code.",
+                                           @"Joe loves to code."]);
+            expect(stringArray).toNot.equal(@[@"Jim", @"Tom", @"Tim", @"Joe"]);
         });
+    });
+    
+    
+    describe(@"selectWithComparator:", ^{
         
-        
-        
-        
+        it(@"Should iterate over an array similiar to the predicateWithBlock: method and return a NSArray.  The argument here is a block which will return a BOOL evaluating the object in self (self being the array calling this method.  The new array will be filetered based upon the impleentation of this method and how it handles the YES/NO being returned.", ^{
+           
+           
+           
+           
+           
+           
+//            NSArray *filteredResultsArray = [resultsArray selectWithComparator:^BOOL(id object) {
+//                
+//                if ([object integerValue]%2 == 0)
+//                {
+//                    return YES;
+//                }
+//                else
+//                {
+//                    return NO;
+//                }
+//            }];
+
+           
+           
+           
+           
+           });
         
         
     });
     
     
-    it(@"", ^{
-        
-    });  
     
-    afterEach(^{
-        
-    });
     
-    afterAll(^{
-        
-    });
+//    - (NSArray *)selectWithComparator:(BOOL (^) (id object))operation {
+//        
+//        NSMutableArray *processingArray = [[NSMutableArray alloc] init];
+//        
+//        for (id object in self)
+//        {
+//            if (operation(object)) {
+//                [processingArray addObject:object];
+//            }
+//        }
+//        
+//        return processingArray;
+//    }
+    
+    
+    
+    
+    
 });
 
 SpecEnd
