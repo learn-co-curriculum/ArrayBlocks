4798357182558230163496f8a122e43b55fbc21f
JimCampagno <jimcampagno@gmail.com>
2015-08-27 16:13:37 -0400

    pod files added

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'arrayBlocks' do
+
+end
+
+target 'arrayBlocksTests' do
+
+  pod 'Specta'
+  pod 'Expecta'
+
+end
+
---
+++ arrayBlocksTests/NSArray+EnumerationSpec.m
@@ -0,0 +1,38 @@
+//
+//  NSArray+EnumerationSpec.m
+//  arrayBlocks
+//
+//  Created by James Campagno on 8/27/15.
+//  Copyright 2015 Zachary Drossman. All rights reserved.
+//
+
+#import "Specta.h"
+#import "NSArray+Enumeration.h"
+
+
+SpecBegin(NSArray)
+
+describe(@"NSArray+Enumeration", ^{
+    
+    beforeAll(^{
+
+    });
+    
+    beforeEach(^{
+
+    });
+    
+    it(@"", ^{
+
+    });  
+    
+    afterEach(^{
+
+    });
+    
+    afterAll(^{
+
+    });
+});
+
+SpecEnd
